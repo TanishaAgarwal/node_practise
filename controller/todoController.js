9f902f5035d93fa617ab9fc3a396cff0b555c387
TanishaAgarwal <tanishaaggarwal2502@gmail.com>
2019-05-28 01:04:00 +0530

    corrections

--- controller/todoController.js
+++ controller/todoController.js
@@ -6,16 +6,18 @@ var urlencoderParser = bodyParser.urlencoded({extended: false});
 module.exports = function(app){
 
     app.get('/todo',function(req,res){
-        res.render('todo',{todos : data});
+        res.render('todo', {todos: data});
     });
 
-    app.post('/todo',urlencoderParser, function(req,res){
+    app.post('/todo', urlencoderParser , function(req,res){
         data.push(req.body);
         res.json(data);
     });
 
     app.delete('/todo/:item',function(req,res){
-        
+        data = data.filter(function(todo){
+            return todo.item.replace(/ /g, '-') !== req.params.item;
+        });
     });
 
-};
+}
