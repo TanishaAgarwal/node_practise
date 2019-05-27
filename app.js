e0fceae6a2c362c1f1ef5880ca300e0665dfc7bc
TanishaAgarwal <tanishaaggarwal2502@gmail.com>
2019-05-27 23:08:09 +0530

    todocontroller

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ app.set('view engine','ejs');
 
 //static files
 app.use('/assets',express.static('./public'));
+
+//port
+app.listen(3000);
+console.log('server is running to-do list on http://127.0.0.1:3000/');
