e7b3c237cc8333386a09578efd2b31cd01b37d8a
TanishaAgarwal <tanishaaggarwal2502@gmail.com>
2019-05-27 23:31:18 +0530

    including ejs

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
+var todoController = require('./controller/todoController');
 
 //set up engine
 app.set('view engine','ejs');
 
 //static files
 app.use('/assets',express.static('./public'));
+
+//fire controller
+todoController(app);
 
 //port
 app.listen(3000);
