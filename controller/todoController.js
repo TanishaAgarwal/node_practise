ec9c4dc1d633906aedde137b602136bc8abe5d8b
TanishaAgarwal <tanishaaggarwal2502@gmail.com>
2019-05-28 00:01:41 +0530

    changes2

--- controller/todoController.js
+++ controller/todoController.js
@@ -1,11 +1,17 @@
+var bodyParser = require('body-parser');
+
+var data = [{item : 'go to walk'},{item:'go to shop'},{item : 'go to home'}];
+var urlencoderParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app){
 
     app.get('/todo',function(req,res){
-        res.render('todo');
+        res.render('todo',{todos : data});
     });
 
-    app.post('/todo',function(req,res){
-        
+    app.post('/todo',urlencoderParser, function(req,res){
+        data.push(req.body);
+        res.json(data);
     });
 
     app.delete('/todo',function(req,res){
